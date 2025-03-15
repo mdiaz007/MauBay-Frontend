dd06ed4b8f2eca4aa795fa07284456cc6ed2bad5
Mauricio Diaz <mauricio809diaz@gmail.com>
2025-03-15 18:50:06 -0400

    added searchbar back, will redirect to marketplace page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import Card from '../components/Card'
 
+import SearchBar from '../components/SearchBar'
+
 function Home(props) {
 
     return (
         <>
             <div className="homepage">
 
+                <SearchBar />
+
                 <h1 className="homepage_categories">Cars | Clothing | Toys | Electronics</h1>
+                <h1 className="homepage_sponsored">Sponsored Items</h1>
 
                 <div className="homepagemiddle">
                     {/* <Card name="ShoppingCard" item="Pagani Zonda" price="6,500,000" imageURL="ZONDA.jpg" /> */}
