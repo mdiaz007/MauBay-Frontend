17f44c63ad926fea6c03f020613e99ba6850a056
Mauricio Diaz <mauricio809diaz@gmail.com>
2025-03-21 22:53:42 -0400

    added function for getting current user's userID

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,6 +6,10 @@ import { useState } from 'react'
 
 import DashboardWindow from '../components/DashboardWindow'
 
+import Session from 'supertokens-auth-react/recipe/session';
+
+
+
 function Dashboard() {
 
     const [value, setValue] = useState("Default Here")
@@ -13,6 +17,17 @@ function Dashboard() {
     function setDashboard(value){
         setValue(value)
     }
+
+    // Need this for getting current user ID, works!
+    async function getJWT() {
+        if (await Session.doesSessionExist()) {
+            let userId = await Session.getUserId();
+            let jwt = await Session.getAccessToken();
+            return userId
+        }
+    }
+
+    // console.log(getJWT())
 
     return (
         <SessionAuth>
